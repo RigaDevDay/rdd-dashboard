b3af119ef6b66f3e0505c065dd0666104982968f
Andrey Adamovich <andrey@aestasit.com>
2015-01-19 01:14:14 +0200

    syntax error

--- jobs/conference.rb
+++ jobs/conference.rb
@@ -5,8 +5,8 @@ attendees = 400
 
 SCHEDULER.every '2s' do
 
-  send_event('tracks', { current: tracks, last: tracks }))
+  send_event('tracks', { current: tracks, last: tracks })
   send_event('sessions', { current: sessions, last: sessions })
-  send_event('attendees', { current: attendees, last: attendees }))
+  send_event('attendees', { current: attendees, last: attendees })
 
 end
