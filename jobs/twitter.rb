1056bc370b786ac7c0a170a0ebe7bd2dd211595b
Andrey Adamovich <andrey@aestasit.com>
2015-01-19 00:37:46 +0200

    working on layout

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,10 +1,13 @@
 require 'twitter'
+require 'yaml'
+
+conference_config = YAML.load_file('rigadevday.yml')
 
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-  config.access_token = 'YOUR_OAUTH_TOKEN'
-  config.access_token_secret = 'YOUR_OAUTH_SECRET'
+  config.consumer_key = conference_config['consumer_key']
+  config.consumer_secret = conference_config['consumer_secret']
+  config.access_token = conference_config['access_token']
+  config.access_token_secret = conference_config['access_token_secret']
 end
 
 search_term = URI::encode('#rigadevday')
@@ -12,7 +15,6 @@ search_term = URI::encode('#rigadevday')
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
     tweets = twitter.search("#{search_term}")
-
     if tweets
       tweets = tweets.map do |tweet|
         { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
