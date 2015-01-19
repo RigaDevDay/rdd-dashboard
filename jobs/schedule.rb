954a53352847a1170ff9b69b1dca5773199db571
Andrey Adamovich <andrey@aestasit.com>
2015-01-19 10:45:53 +0200

    fixing schedule retrieval job

--- jobs/schedule.rb
+++ jobs/schedule.rb
@@ -27,7 +27,7 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
   end
   current_slot = time_slots.select { |time_slot| to_min(time_slot[:time_code]) > current_min + 15 }.first
   if current_slot
-    sessions = current_slot['events'].map do |event|
+    sessions = current_slot[:events].map do |event|
       { name: event['subtitle'], body: event['description'] }
     end
     send_event('schedule', sessions: sessions)
