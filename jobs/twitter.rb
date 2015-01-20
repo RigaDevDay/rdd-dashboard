251929b5de2ad232046aca62085124d7bc01d88c
Andrey Adamovich <andrey@aestasit.com>
2015-01-20 16:50:03 +0200

    integrating twitter and voting stats

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -18,14 +18,18 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
   begin
     tweets = twitter.search("#{search_term}", { :result_type => 'recent', :count => 100 })
     # TODO: add all tweets to database
-    # TODO: select tweet stats from database
-    # TODO: send data to dashing
     if tweets
       tweets = tweets.select { |tweet| !tweet.text.start_with?('RT') }.take(20).map do |tweet|
         { name: tweet.user.name, time: tweet.created_at.in_time_zone('Europe/Riga').strftime("%m-%d %H:%M:%S"), body: tweet.text, avatar: tweet.user.profile_image_url_https }
       end
       send_event('twitter_mentions', comments: tweets)
     end
+    stats = []
+    # TODO: select tweet stats from database
+    (1..24).each do |i|
+      stats << { x: i, y: rand(50) }
+    end
+    send_event('twitter_activity', points: stats)
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
   end
