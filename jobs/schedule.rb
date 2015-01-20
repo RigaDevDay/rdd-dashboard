d4a4823d3ac242274e022f4bf4a9f0e150f2f1cc
Andrey Adamovich <andrey@aestasit.com>
2015-01-20 20:17:15 +0200

    different room names

--- jobs/schedule.rb
+++ jobs/schedule.rb
@@ -22,27 +22,28 @@ end
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
   current_time = Time.now.in_time_zone('Europe/Riga')
-  current_min = current_time.hour * 60 + current_time.min
+  current_min  = current_time.hour * 60 + current_time.min
   time_slots   = schedule.map do |time_slot|
     { :time => time_slot['time'], :time_code => time_slot['time'].split(':').join('').to_i, :events => time_slot['events'] }
   end
   current_slot = time_slots.select { |time_slot| to_min(time_slot[:time_code]) > current_min + 15 }.first
   if current_slot
-    sessions = current_slot[:events].each_with_index.map do |event, i|
+    sessions       = current_slot[:events].each_with_index.map do |event, i|
       speaker_id   = event['speakers'] ? event['speakers'].first : nil
       avatar       = speaker_id ? "https://raw.githubusercontent.com/RigaDevDay/RigaDevDay.github.io/master/assets/img/speaker-photos/#{speaker_id}.png" : nil
       room_name    = rooms[i]
       if !avatar && event['title']
+        room_name = 'HALLWAY'
         if event['title'].include? "Coffee"
-          avatar   = '/assets/coffee_break.png'
+          avatar    = '/assets/coffee_break.png'
         elsif event['title'].include? "Lunch"
-          avatar   = '/assets/lunch.png'
+          avatar    = '/assets/lunch.png'
         elsif event['title'].include? "Afterparty"
-          avatar   = '/assets/party.png'
+          avatar    = '/assets/party.png'
+          room_name = 'RIGA CITY'
         else
-          avatar   = '/assets/favicon.png'
+          avatar    = '/assets/favicon.png'
         end          
-        room_name  = 'M. Hall'
       end
       speaker      = speaker_id ? speaker_data.find { |speaker| "#{speaker['id']}" == "#{speaker_id}" } : nil
       speaker_name = speaker ? speaker['name'] : ''
