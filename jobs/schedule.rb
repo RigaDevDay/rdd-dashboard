49c0cd1a9bb7c15c82c367d4c4333670069ad305
Andrey Adamovich <andrey@aestasit.com>
2017-05-12 21:13:32 +0300

    commented out code to test deployment

--- jobs/schedule.rb
+++ jobs/schedule.rb
@@ -4,73 +4,73 @@ require 'net/http'
 require 'active_support/time'
 require 'uri'
 
-github_uri       = URI.parse('https://raw.githubusercontent.com/')
-http             = Net::HTTP.new(github_uri.host, github_uri.port)
-http.use_ssl     = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-event_data       = JSON.parse(http.get('/RigaDevDay/RigaDevDay.github.io/master/assets/data/main.json').body)
-speaker_data     = event_data['speakers']
-schedule_data    = event_data['days'][Date.today.day % 2]
-
-schedule         = schedule_data['schedule']['schedule']
-rooms            = schedule_data['schedule']['roomNames']
-
-def to_min(time_code)
-  return (time_code / 100) * 60 + time_code % 100
-end
-
-
-SCHEDULER.every '1m', :first_in => 0 do |job|
-  current_time = Time.now.in_time_zone('Europe/Riga')
-  current_min  = current_time.hour * 60 + current_time.min
-  time_slots   = schedule.map do |time_slot|
-    { :time => time_slot['time'], :time_code => time_slot['time'].split(':').join('').to_i, :events => time_slot['events'] }
-  end
-  current_slot = time_slots.select { |time_slot| to_min(time_slot[:time_code]) > current_min + 15 }.first
-  current_slot ||= time_slots.last
-  if current_slot
-    sessions       = current_slot[:events].each_with_index.map do |event, i|
-      speaker_id   = event['speakers'] ? event['speakers'].first : nil
-      speaker      = speaker_id ? speaker_data.find { |speaker| "#{speaker['id']}" == "#{speaker_id}" } : nil
-      avatar       = speaker ? "https://raw.githubusercontent.com/RigaDevDay/RigaDevDay.github.io/master/#{speaker['img']}" : nil
-      room_name    = rooms[i]
-      if event['subtitle'].include? "Open Source and OpenJDK"
-        room_name  = 'Room 2'
-      end
-      if event['subtitle'].include? "The Web - What it Has"
-        room_name  = 'Room 2'
-      end
-      if !avatar && event['title']
-        room_name = 'HALL'
-        if event['title'].include? "Coffee"
-          avatar    = '/assets/coffee_break.png'
-        elsif event['title'].include? "Lunch"
-          avatar    = '/assets/lunch.png'
-        elsif event['title'].include? "Closing"
-          avatar    = '/assets/favicon.png'
-          room_name = 'Room 2'
-        elsif event['title'].include? "Opening"
-          avatar    = '/assets/favicon.png'
-          room_name = 'Room 2'
-        elsif event['title'].include? "Afterparty"
-          avatar    = '/assets/party.png'
-        else
-          avatar    = '/assets/favicon.png'
-        end          
-      end
-      speaker      = speaker_id ? speaker_data.find { |speaker| "#{speaker['id']}" == "#{speaker_id}" } : nil
-      speaker_name = speaker ? speaker['name'] : ''
-      {
-         title: "#{event['title']} #{event['subtitle']}",
-         description: event['description'],
-         avatar: avatar,
-         author: speaker_name,
-         time: current_slot[:time],
-         room: room_name
-      }
-    end
-    send_event('schedule', sessions: sessions)
-  end
-end
-
+# github_uri       = URI.parse('https://raw.githubusercontent.com/')
+# http             = Net::HTTP.new(github_uri.host, github_uri.port)
+# http.use_ssl     = true
+# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#
+# event_data       = JSON.parse(http.get('/RigaDevDay/RigaDevDay.github.io/master/assets/data/main.json').body)
+# speaker_data     = event_data['speakers']
+# schedule_data    = event_data['days'][Date.today.day % 2]
+#
+# schedule         = schedule_data['schedule']['schedule']
+# rooms            = schedule_data['schedule']['roomNames']
+#
+# def to_min(time_code)
+#   return (time_code / 100) * 60 + time_code % 100
+# end
+#
+#
+# SCHEDULER.every '1m', :first_in => 0 do |job|
+#   current_time = Time.now.in_time_zone('Europe/Riga')
+#   current_min  = current_time.hour * 60 + current_time.min
+#   time_slots   = schedule.map do |time_slot|
+#     { :time => time_slot['time'], :time_code => time_slot['time'].split(':').join('').to_i, :events => time_slot['events'] }
+#   end
+#   current_slot = time_slots.select { |time_slot| to_min(time_slot[:time_code]) > current_min + 15 }.first
+#   current_slot ||= time_slots.last
+#   if current_slot
+#     sessions       = current_slot[:events].each_with_index.map do |event, i|
+#       speaker_id   = event['speakers'] ? event['speakers'].first : nil
+#       speaker      = speaker_id ? speaker_data.find { |speaker| "#{speaker['id']}" == "#{speaker_id}" } : nil
+#       avatar       = speaker ? "https://raw.githubusercontent.com/RigaDevDay/RigaDevDay.github.io/master/#{speaker['img']}" : nil
+#       room_name    = rooms[i]
+#       if event['subtitle'].include? "Open Source and OpenJDK"
+#         room_name  = 'Room 2'
+#       end
+#       if event['subtitle'].include? "The Web - What it Has"
+#         room_name  = 'Room 2'
+#       end
+#       if !avatar && event['title']
+#         room_name = 'HALL'
+#         if event['title'].include? "Coffee"
+#           avatar    = '/assets/coffee_break.png'
+#         elsif event['title'].include? "Lunch"
+#           avatar    = '/assets/lunch.png'
+#         elsif event['title'].include? "Closing"
+#           avatar    = '/assets/favicon.png'
+#           room_name = 'Room 2'
+#         elsif event['title'].include? "Opening"
+#           avatar    = '/assets/favicon.png'
+#           room_name = 'Room 2'
+#         elsif event['title'].include? "Afterparty"
+#           avatar    = '/assets/party.png'
+#         else
+#           avatar    = '/assets/favicon.png'
+#         end
+#       end
+#       speaker      = speaker_id ? speaker_data.find { |speaker| "#{speaker['id']}" == "#{speaker_id}" } : nil
+#       speaker_name = speaker ? speaker['name'] : ''
+#       {
+#          title: "#{event['title']} #{event['subtitle']}",
+#          description: event['description'],
+#          avatar: avatar,
+#          author: speaker_name,
+#          time: current_slot[:time],
+#          room: room_name
+#       }
+#     end
+#     send_event('schedule', sessions: sessions)
+#   end
+# end
+#
--- jobs/voting.rb
+++ jobs/voting.rb
@@ -4,38 +4,38 @@ require 'net/http'
 require 'active_support/time'
 require 'uri'
 
-github_uri       = URI.parse('https://raw.githubusercontent.com/')
-http             = Net::HTTP.new(github_uri.host, github_uri.port)
-http.use_ssl     = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-event_data       = JSON.parse(http.get('/RigaDevDay/RigaDevDay.github.io/master/assets/data/main.json').body)
-speaker_data     = event_data['speakers']
-schedule_data    = event_data['days'][Date.today.day % 2]
-schedule_data2   = event_data['days'][Date.today.day % 2 == 0 ? 1 : 0]
-
-schedule         = schedule_data['schedule']['schedule']
-schedule2        = schedule_data2['schedule']['schedule']
-sessions         = schedule.map { |time_slot| time_slot['events'] }.flatten
-all_sessions     = schedule2.map { |time_slot| time_slot['events'] }.flatten.concat(sessions)
-
-SCHEDULER.every '2m', :first_in => 0 do |job|
-  voting_data    = JSON.parse(Net::HTTP.get(URI.parse('http://dmi3.net/plusminus/?top')))
-  votes          = voting_data.map do |session|
-    total_votes  = session['neutral'] + session['plus'] + session['minus']
-    mark         = session['neutral'] + 2 * session['plus'] - session['minus']    
-    rate         = (mark != 0 ? mark / (2 * total_votes) : 0) * 100
-    session_item = all_sessions.find { |s| s['subtitle'] == session['title'] }
-    speaker_id   = session_item ? session_item['speakers'].first : nil
-    speaker      = speaker_id ? speaker_data.find { |speaker| "#{speaker['id']}" == "#{speaker_id}" } : nil
-    speaker_name = speaker ? speaker['name'] : ''
-    avatar       = speaker ? "https://raw.githubusercontent.com/RigaDevDay/RigaDevDay.github.io/master/#{speaker['img']}" : nil
-    { title: session['title'], avatar: avatar, author: speaker_name, rate: rate, total: total_votes }
-  end
-  votes.sort { |s1, s2| s2[:rate] <=> s1[:rate] }.each { |session| puts "#{session[:title]}: #{session[:rate]} * #{session[:total]}" }
-  votes          = votes.select { |session| session[:rate] > 0 && session[:total] > 10 }.sort { |s1, s2| s2[:rate] <=> s1[:rate] }.take(6)
-  votes.each { |session| session[:rate] = "%.2f%%" % session[:rate] }
-  send_event('voting', votes: votes)
-end
-
+# github_uri       = URI.parse('https://raw.githubusercontent.com/')
+# http             = Net::HTTP.new(github_uri.host, github_uri.port)
+# http.use_ssl     = true
+# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#
+# event_data       = JSON.parse(http.get('/RigaDevDay/RigaDevDay.github.io/master/assets/data/main.json').body)
+# speaker_data     = event_data['speakers']
+# schedule_data    = event_data['days'][Date.today.day % 2]
+# schedule_data2   = event_data['days'][Date.today.day % 2 == 0 ? 1 : 0]
+#
+# schedule         = schedule_data['schedule']['schedule']
+# schedule2        = schedule_data2['schedule']['schedule']
+# sessions         = schedule.map { |time_slot| time_slot['events'] }.flatten
+# all_sessions     = schedule2.map { |time_slot| time_slot['events'] }.flatten.concat(sessions)
+#
+# SCHEDULER.every '2m', :first_in => 0 do |job|
+#   voting_data    = JSON.parse(Net::HTTP.get(URI.parse('http://dmi3.net/plusminus/?top')))
+#   votes          = voting_data.map do |session|
+#     total_votes  = session['neutral'] + session['plus'] + session['minus']
+#     mark         = session['neutral'] + 2 * session['plus'] - session['minus']
+#     rate         = (mark != 0 ? mark / (2 * total_votes) : 0) * 100
+#     session_item = all_sessions.find { |s| s['subtitle'] == session['title'] }
+#     speaker_id   = session_item ? session_item['speakers'].first : nil
+#     speaker      = speaker_id ? speaker_data.find { |speaker| "#{speaker['id']}" == "#{speaker_id}" } : nil
+#     speaker_name = speaker ? speaker['name'] : ''
+#     avatar       = speaker ? "https://raw.githubusercontent.com/RigaDevDay/RigaDevDay.github.io/master/#{speaker['img']}" : nil
+#     { title: session['title'], avatar: avatar, author: speaker_name, rate: rate, total: total_votes }
+#   end
+#   votes.sort { |s1, s2| s2[:rate] <=> s1[:rate] }.each { |session| puts "#{session[:title]}: #{session[:rate]} * #{session[:total]}" }
+#   votes          = votes.select { |session| session[:rate] > 0 && session[:total] > 10 }.sort { |s1, s2| s2[:rate] <=> s1[:rate] }.take(6)
+#   votes.each { |session| session[:rate] = "%.2f%%" % session[:rate] }
+#   send_event('voting', votes: votes)
+# end
+#
 
