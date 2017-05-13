61bad4a8395aa26f384f9a79e0e2ffa4a47d2445
Andrey Adamovich <andrey@aestasit.com>
2017-05-13 21:53:53 +0300

    fixed voting calculations

--- jobs/voting.rb
+++ jobs/voting.rb
@@ -11,46 +11,49 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   speakers      = firebase.get('speakers').body
   feedback      = firebase.get('userFeedbacks').body
 
-  votes         = sessions.select { |session_data| session_data['speakers'] }.map do |session_id, session_data|
-    speaker     = speakers[session_data['speakers'].first]
-    {
-        title: session_data['title'],
-        avatar: "https://rigadevdays.lv/#{speaker['photoUrl']}",
-        author: speaker['name'],
-        speakerPoints: 0.0,
-        speakerVotes: 0.0,
-        contentPoints: 0.0,
-        contentVotes: 0.0
-    }
-  end
+  voting_data   = Hash[
+    sessions.select { |session_id, session_data| session_data.key?('speakers') }.collect do |session_id, session_data|
+      speaker     = speakers[session_data['speakers'].first]
+      [
+        session_id,
+        {
+            title: session_data['title'],
+            avatar: "https://rigadevdays.lv/#{speaker['photoUrl']}",
+            author: speaker['name'],
+            speakerPoints: 0.0,
+            speakerVotes: 0.0,
+            contentPoints: 0.0,
+            contentVotes: 0.0,
+            rate: 0.0
+        }
+      ]
+    end
+  ]
 
-  feedback.each do |user_id, sessionFeedback|
-    sessionFeedback.each do |session_id, ratings|
-      if ratings['qualityOfContent']
-
+  feedback.each do |user_id, session_feedback|
+    session_feedback.each do |session_id, ratings|
+      session_data = voting_data[session_id.to_s]
+      if ratings.key?('qualityOfContent') && ['qualityOfContent']
+        session_data[:contentPoints] += ratings['qualityOfContent'].to_i
+        session_data[:contentVotes] += 1
       end
-      if ratings['speakerPerformance']
-
+      if ratings.key?('speakerPerformance') && ratings['speakerPerformance']
+        session_data[:speakerPoints] += ratings['speakerPerformance'].to_i
+        session_data[:speakerVotes] += 1
       end
-
+      session_data[:totalVotes] = session_data[:speakerVotes] + session_data[:contentVotes]
+      session_data[:rate] = session_data[:totalVotes] > 0 ? (session_data[:speakerPoints] + session_data[:contentPoints]) / session_data[:totalVotes] : 0.0
     end
   end
 
-  # voting_data    = JSON.parse(Net::HTTP.get(URI.parse('http://dmi3.net/plusminus/?top')))
-  # votes          = voting_data.map do |session|
-  #   total_votes  = session['neutral'] + session['plus'] + session['minus']
-  #   mark         = session['neutral'] + 2 * session['plus'] - session['minus']
-  #   rate         = (mark != 0 ? mark / (2 * total_votes) : 0) * 100
-  #   session_item = all_sessions.find { |s| s['subtitle'] == session['title'] }
-  #   speaker_id   = session_item ? session_item['speakers'].first : nil
-  #   speaker      = speaker_id ? speaker_data.find { |speaker| "#{speaker['id']}" == "#{speaker_id}" } : nil
-  #   speaker_name = speaker ? speaker['name'] : ''
-  #   avatar       = speaker ? "https://raw.githubusercontent.com/RigaDevDay/RigaDevDay.github.io/master/#{speaker['img']}" : nil
-  #   { title: session['title'], avatar: avatar, author: speaker_name, rate: rate, total: total_votes }
-  # end
-  # votes.sort { |s1, s2| s2[:rate] <=> s1[:rate] }.each { |session| puts "#{session[:title]}: #{session[:rate]} * #{session[:total]}" }
-  # votes          = votes.select { |session| session[:rate] > 0 && session[:total] > 10 }.sort { |s1, s2| s2[:rate] <=> s1[:rate] }.take(6)
-  # votes.each { |session| session[:rate] = "%.2f%%" % session[:rate] }
+  votes = voting_data
+              .map { |session_id, session_data| session_data }
+              .select { |session_data| session_data[:rate] > 0 }
+              .select { |session_data| session_data[:totalVotes] > 0 }
+              .sort { |s1, s2| s2[:rate] <=> s1[:rate] }
+              .take(6)
+
+  votes.each { |session| session[:rate] = "%.2f" % session[:rate] }
 
   send_event('voting', votes: votes)
 
