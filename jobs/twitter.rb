e2fb4b3716abcc4d657a827b22eb259de455267f
Andrey Adamovich <andrey@aestasit.com>
2017-05-15 14:30:13 +0300

    removed poop

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -166,7 +166,7 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
 
     # Send most recent 18 tweets (excluding retweets) to dashboard.
     if tweets
-      tweets = tweets.select { |tweet| !tweet.text.start_with?('RT') && tweet.name.downcase.include?('poop') }.take(18).map do |tweet|
+      tweets = tweets.select { |tweet| !tweet.text.start_with?('RT') && tweet.user.name.downcase.include?('poop') }.take(18).map do |tweet|
         {
           name:      tweet.user.name,
           avatar:    "#{tweet.user.profile_image_url_https}",
@@ -258,7 +258,6 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
         tweet_count: row[0]
       }
     end
-    exclude_users = global_config['twitter_exclude_heroes'] || [ 'PoopEmoji' ]
     top_users = top_users.select { |user| !(user['name'] && user['name'].downcase.include?('poop')) }
     unless top_users.empty?
       send_event('twitter_top_users', {users: top_users.take(6)})
